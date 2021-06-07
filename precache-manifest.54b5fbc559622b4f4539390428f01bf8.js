0cfe7ba3629ab47a8d42bee950390a5d636ab74e
Amit <khurjaamit@gmail.com>
2021-06-07 23:54:03 +0530

    Updates

---
+++ precache-manifest.54b5fbc559622b4f4539390428f01bf8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0bb4814eb51b736e124e13cb3f925f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed545291f6ddd7f28ab2",
+    "url": "/static/css/main.eb145df2.chunk.css"
+  },
+  {
+    "revision": "27ce1c178e8100b4c1a6",
+    "url": "/static/js/2.77cd9d7f.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.77cd9d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed545291f6ddd7f28ab2",
+    "url": "/static/js/main.5ce88853.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "02208489e7095f5e1c600f67774398da",
+    "url": "/static/media/amit.02208489.png"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
